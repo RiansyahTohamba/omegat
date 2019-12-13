b244f2e220c8dc6c5f9526b5d8f53ab76c376080
muhammad riansyah <mriansyah93@gmail.com>
2019-12-13 10:13:01 +0700

    extract DependOnMainWindow and update refactoring notes

---
+++ src/org/omegat/core/DependOnMainWindow.java
@@ -0,0 +1,37 @@
+package org.omegat.core;
+
+import org.omegat.gui.comments.CommentsTextArea;
+import org.omegat.gui.dictionaries.DictionariesTextArea;
+import org.omegat.gui.editor.EditorController;
+import org.omegat.gui.exttrans.MachineTranslateTextArea;
+import org.omegat.gui.glossary.GlossaryManager;
+import org.omegat.gui.glossary.GlossaryTextArea;
+import org.omegat.gui.issues.IssuesPanelController;
+import org.omegat.gui.main.MainWindow;
+import org.omegat.gui.matches.MatchesTextArea;
+import org.omegat.gui.multtrans.MultipleTransPane;
+import org.omegat.gui.notes.NotesTextArea;
+import org.omegat.gui.properties.SegmentPropertiesArea;
+
+public class DependOnMainWindow {
+    static void dependOnMainWindow() {
+        /*        TODO: how to separate these objects?  */
+        MainWindow me = new MainWindow();
+        Core.mainWindow = me;
+
+        Core.editor = new EditorController(me);
+        Core.issuesWindow = new IssuesPanelController(me);
+        Core.matcher = new MatchesTextArea(me);
+        Core.notes = new NotesTextArea(me);
+        Core.comments = new CommentsTextArea(me);
+        Core.machineTranslatePane = new MachineTranslateTextArea(me);
+        Core.dictionaries = new DictionariesTextArea(me);
+        Core.multiple = new MultipleTransPane(me);
+
+        GlossaryTextArea glossaryArea = new GlossaryTextArea(me);
+        Core.glossary = glossaryArea;
+        Core.glossaryManager = new GlossaryManager(glossaryArea);
+
+        new SegmentPropertiesArea(me);
+    }
+}
