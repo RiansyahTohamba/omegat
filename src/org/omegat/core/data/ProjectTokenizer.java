59a5a7a1971bc357a491182e072c6d00146b3b7d
muhammad riansyah <mriansyah93@gmail.com>
2020-01-02 11:38:21 +0700

    add 3 method additional to ProjectTokenizer

--- src/org/omegat/core/data/ProjectTokenizer.java
+++ src/org/omegat/core/data/ProjectTokenizer.java
@@ -1,11 +1,55 @@
-package org.omegat.core.data;public class ProjectTokenizer{private final org.omegat.core.data.RealProject realProject;	public ProjectTokenizer(org.omegat.core.data.RealProject realProject)	{		this.realProject = realProject;	}/**
-     * {@inheritDoc}
+package org.omegat.core.data;
+
+import org.omegat.CLIParameters;
+import org.omegat.core.Core;
+import org.omegat.tokenizer.DefaultTokenizer;
+import org.omegat.tokenizer.ITokenizer;
+import org.omegat.util.Log;
+import org.omegat.util.StringUtil;
+
+public class ProjectTokenizer {
+
+    public ITokenizer getTarget(ProjectProperties props) {
+        ITokenizer targetTokenizer = createTokenizer(Core.getParams().get(CLIParameters.TOKENIZER_TARGET),
+                props.getTargetTokenizer());
+        Log.log("Target tokenizer: " + targetTokenizer.getClass().getName());
+        return targetTokenizer;
+    }
+
+    public ITokenizer getSource(ProjectProperties props) {
+        ITokenizer sourceTokenizer = createTokenizer(Core.getParams().get(CLIParameters.TOKENIZER_SOURCE),
+                props.getSourceTokenizer());
+        Log.log("Source tokenizer: " + sourceTokenizer.getClass().getName());
+        return sourceTokenizer;
+    }
+    /**
+     * Create tokenizer class. Classes are prioritized:
+     * <ol><li>Class specified on command line via <code>--ITokenizer</code>
+     * and <code>--ITokenizerTarget</code></li>
+     * <li>Class specified in project settings</li>
+     * <li>{@link DefaultTokenizer}</li>
+     * </ol>
+     *
+     * @param cmdLine Tokenizer class specified on command line
+     * @return Tokenizer implementation
      */
-    public org.omegat.tokenizer.ITokenizer getSourceTokenizer() {
-        return realProject.getSourceTokenizer();
-    }/**
-     * {@inheritDoc}
-     */
-    public org.omegat.tokenizer.ITokenizer getTargetTokenizer() {
-        return realProject.getTargetTokenizer();
-    }}
+    protected ITokenizer createTokenizer(String cmdLine, Class<?> projectPref) {
+        if (!StringUtil.isEmpty(cmdLine)) {
+            try {
+                return (ITokenizer) this.getClass().getClassLoader().loadClass(cmdLine).getDeclaredConstructor()
+                        .newInstance();
+            } catch (ClassNotFoundException e) {
+                Log.log(e.toString());
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            return (ITokenizer) projectPref.getDeclaredConstructor().newInstance();
+        } catch (Throwable e) {
+            Log.log(e);
+        }
+
+        return new DefaultTokenizer();
+    }
+}
