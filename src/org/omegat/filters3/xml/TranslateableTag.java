5fb3f364b8743fe5890a5903824cf87db6deea98
muhammad riansyah <mriansyah93@gmail.com>
2020-01-06 14:35:16 +0700

    update TranslateableTag & TranslationHandler

--- src/org/omegat/filters3/xml/TranslateableTag.java
+++ src/org/omegat/filters3/xml/TranslateableTag.java
@@ -2,8 +2,42 @@ package org.omegat.filters3.xml;
 
 import java.util.Stack;
 
-public class TranslateableTagName {
+public class TranslateableTag {
     /** Name of the current variable translatable tag */
     Stack<String> translatableTagName = new Stack<String>();
 
+    /**
+     * If we are not inside a translatable tag, and if the dialect says the new
+     * one is translatable, add the new tag to the stack
+     *
+     * @param tag
+     *            The current opening tag
+     * @param atts
+     *            The attributes of the current tag
+     */
+    // TODO: The concept works only perfectly if the first tag with
+    // translatable content inside the translatable tag is a paragraph
+    // tag
+    void setTranslatableTag(String tag, org.omegat.filters3.Attributes atts,XMLDialect dialect) {
+
+        if (!isTranslatableTag()) { // If stack is empty
+            if (dialect.validateTranslatableTag(tag, atts)) {
+                translatableTagName.push(tag);
+            }
+        } else {
+            translatableTagName.push(tag);
+        }
+    }
+
+    /**
+     * Remove a tag from the stack of translatable tags
+     */
+    void removeTranslatableTag() {
+        if (isTranslatableTag()) { // If there is something in the stack
+            translatableTagName.pop(); // Remove it
+        }
+    }
+    private boolean isTranslatableTag() {
+        return !translatableTagName.empty();
+    }
 }
--- src/org/omegat/filters3/xml/TranslationHandler.java
+++ src/org/omegat/filters3/xml/TranslationHandler.java
@@ -30,88 +30,9 @@ public class TranslationHandler {
 
     /** Keep the attributes of an intact tag. */
     org.omegat.filters3.Attributes intacttagAttributes = null;
-    /**
-     * Is called when the tag is started.
-     */
-    void start(String tag, Attributes attributes) throws SAXException, TranslationException {
-        boolean prevIgnored = handler.getTranslator().isInIgnored();
-        translatorTagStart(tag, attributes);
 
-        if (!handler.getTranslator().isInIgnored()) {
-            if (handler.isOutOfTurnTag(tag)) {
-                XMLOutOfTurnTag ootTag = new XMLOutOfTurnTag(handler.getDialect(), handler, tag, handler.getShortcut(tag), attributes);
-                currEntry().add(ootTag);
-                handler.getOutofturnEntries().push(ootTag.getEntry());
-            } else {
-                if (handler.isParagraphTag(tag, XMLUtils.convertAttributes(attributes)) && !handler.collectingOutOfTurnText()
-                        && !handler.collectingIntactText()) {
-                    translateAndFlush();
-                }
-                queueTag(tag, attributes);
-            }
-        } else {
-            if (!prevIgnored) {
-                // start ignored from this tags - need to flush translation
-                translateAndFlush();
-            }
-            queueIgnoredTag(tag, attributes);
-        }
-    }
 
-    /**
-     * Is called when the tag is ended.
-     */
-    void end(String tag) throws SAXException, TranslationException {
-        boolean prevIgnored = handler.getTranslator().isInIgnored();
-        if (!handler.getTranslator().isInIgnored()) {
-            if (handler.collectingIntactText() && tag.equals(handler.getIntacttagName())
-                    && (isIntactTag(tag, null) || isContentBasedTag(tag, null))) {
-                entryHandler.setIntacttagEntry(null);
-                intacttagName = null;
-                intacttagAttributes = null;
-                handler.removeTranslatableTag();
-            } else if (handler.collectingOutOfTurnText() && handler.isOutOfTurnTag(tag)) {
-                translateButDontFlash();
-                handler.getOutofturnEntries().pop();
-            } else {
-                queueEndTag(tag);
-                // TODO: If a file doesn't contain any paragraph tag,
-                // the translatable content will be lost
-                if (handler.isParagraphTag(tag) && !handler.collectingOutOfTurnText() && !handler.collectingIntactText()) {
-                    translateAndFlush();
-                }
-                handler.removeTranslatableTag();
-            }
-        } else {
-            queueEndTag(tag);
-        }
 
-        translatorTagEnd(tag);
-        if (!handler.getTranslator().isInIgnored() && prevIgnored) {
-            // stop ignored from this tag - need to flush without translate
-            handler.flushButDontTranslate();
-        }
-    }
-    private void queueEndTag(String tag) {
-        int len = currEntry().size();
-        if (len > 0
-                && (currEntry().get(len - 1) instanceof XMLTag)
-                && (((XMLTag) currEntry().get(len - 1)).getTag().equals(tag) && ((XMLTag) currEntry().get(
-                len - 1)).getType() == Tag.Type.BEGIN) && !isClosingTagRequired()) {
-            if (((XMLTag) currEntry().get(len - 1)).getTag().equals(xmlTagName.lastElement())) {
-                xmlTagName.pop();
-                xmlTagAttributes.pop();
-            }
-            ((XMLTag) currEntry().get(len - 1)).setType(Tag.Type.ALONE);
-        } else {
-            XMLTag xmltag = new XMLTag(tag, getShortcut(tag), Tag.Type.END, null, this.translator.getTargetLanguage());
-            if (xmltag.getTag().equals(xmlTagName.lastElement())) {
-                xmlTagName.pop();
-                xmltag.setStartAttributes(xmlTagAttributes.pop()); // Restore attributes
-            }
-            currEntry().add(xmltag);
-        }
-    }
 
     /**
      * One of the main methods of the XML filter: it collects all the data,
@@ -178,21 +99,9 @@ public class TranslationHandler {
         return path.toString();
     }
 
-    void translatorTagStart(String tag, Attributes atts) {
-        handler.getCurrentTagPath().push(tag);
-        handler.getTranslator().tagStart(constructCurrentPath(), atts);
-    }
-
-    void translatorTagEnd(String tag) {
-        handler.getTranslator().tagEnd(constructCurrentPath());
-        while (!handler.getCurrentTagPath().pop().equals(tag)) {
-        }
-    }
 
 
-    private boolean isTranslatableTag() {
-        return !handler.getTranslatableTagName().empty();
-    }
+
 
     private boolean isSpacePreservingTag() {
         if (Core.getFilterMaster().getConfig().isPreserveSpaces()) { // Preserve spaces for all tags
@@ -204,52 +113,6 @@ public class TranslationHandler {
 
     private void resetSpacePreservingTag() {
         spacePreserve = false;
-    }
-
-    /** Now we collect intact text. */
-
-    private void queueTag(String tag, Attributes attributes) {
-        Tag xmltag = null;
-        XMLIntactTag intacttag = null;
-        setTranslatableTag(tag, XMLUtils.convertAttributes(attributes));
-        setSpacePreservingTag(XMLUtils.convertAttributes(attributes));
-        if (!collectingIntactText()) {
-            if (isContentBasedTag(tag, XMLUtils.convertAttributes(attributes))) {
-                intacttag = new XMLContentBasedTag(dialect, this, tag, getShortcut(tag), dialect
-                        .getContentBasedTags().get(tag), attributes);
-                xmltag = intacttag;
-                intacttagName = tag;
-                intacttagAttributes = XMLUtils.convertAttributes(attributes);
-            } else if (isIntactTag(tag, XMLUtils.convertAttributes(attributes))) {
-                intacttag = new XMLIntactTag(dialect, this, tag, getShortcut(tag), attributes);
-                xmltag = intacttag;
-                intacttagName = tag;
-                intacttagAttributes = XMLUtils.convertAttributes(attributes);
-            }
-        }
-        if (xmltag == null) {
-            xmltag = new XMLTag(tag, getShortcut(tag), Tag.Type.BEGIN, attributes, this.translator.getTargetLanguage());
-            xmlTagName.push(xmltag.getTag());
-            xmlTagAttributes.push(xmltag.getAttributes());
-        }
-        currEntry().add(xmltag);
-
-        if (intacttag != null) {
-            entryHandler.setIntacttagEntry(intacttag.getIntactContents());
-        }
-
-        if (!collectingIntactText()) {
-            for (int i = 0; i < xmltag.getAttributes().size(); i++) {
-                Attribute attr = xmltag.getAttributes().get(i);
-                if ((dialect.getTranslatableAttributes().contains(attr.getName()) || dialect
-                        .getTranslatableTagAttributes().containsPair(tag, attr.getName()))
-                        && dialect.validateTranslatableTagAttribute(tag, attr.getName(),
-                        xmltag.getAttributes())) {
-                    attr.setValue(StringUtil.makeValidXML(
-                            translator.translate(StringUtil.unescapeXMLEntities(attr.getValue()), null)));
-                }
-            }
-        }
     }
 
     /**
