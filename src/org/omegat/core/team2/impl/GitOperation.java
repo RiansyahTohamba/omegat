413a3dfa74e213bbeda2470ab680d1a59f5b8985
muhammad riansyah <mriansyah93@gmail.com>
2020-01-07 14:19:13 +0700

    move method to gitoperation

--- src/org/omegat/core/team2/impl/GitOperation.java
+++ src/org/omegat/core/team2/impl/GitOperation.java
@@ -1,23 +1,78 @@
 package org.omegat.core.team2.impl;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.TransportException;
-import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheIterator;
+import org.eclipse.jgit.lib.*;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.transport.PushResult;
 import org.eclipse.jgit.transport.RemoteRefUpdate;
+import org.eclipse.jgit.treewalk.AbstractTreeIterator;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.omegat.core.team2.IRemoteRepository2;
 import org.omegat.util.Log;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-public class GitCommit {
+public class GitOperation {
+    private static final Logger LOGGER = Logger.getLogger(GITRemoteRepository2.class.getName());
+    protected static final String LOCAL_BRANCH = "master";
+    protected static final String REMOTE_BRANCH = "origin/master";
+    protected static final String REMOTE = "origin";
+    protected static final int TIMEOUT = 30; // seconds
+
+    public boolean indexIsEmpty(DirCache dc) throws Exception {
+
+        DirCacheIterator dci = new DirCacheIterator(dc);
+        AbstractTreeIterator old = prepareTreeParser(repository, repository.resolve(Constants.HEAD));
+        try (Git git = new Git(repository)) {
+            List<DiffEntry> diffs = git.diff().setOldTree(old).setNewTree(dci).call();
+            return diffs.isEmpty();
+        }
+    }
+    private static AbstractTreeIterator prepareTreeParser(Repository repository, ObjectId objId) throws Exception {
+        // from the commit we can build the tree which allows us to construct
+        // the TreeParser
+        try (RevWalk walk = new RevWalk(repository)) {
+            RevCommit commit = walk.parseCommit(objId);
+            RevTree tree = walk.parseTree(commit.getTree().getId());
+            CanonicalTreeParser treeParser = new CanonicalTreeParser();
+            ObjectReader reader = repository.newObjectReader();
+            treeParser.reset(reader, tree.getId());
+            return treeParser;
+        }
+    }
+    public void gitSwitchVersion(String version, Git git) throws GitAPIException {
+        if (version == null) {
+            version = REMOTE_BRANCH;
+            // TODO fetch
+            git.fetch().setRemote(REMOTE).setTimeout(TIMEOUT).call();
+        }
+        Log.logDebug(LOGGER, "GIT switchToVersion {0} ", version);
+        git.reset().setMode(ResetCommand.ResetType.HARD).call();
+        git.checkout().setName(version).call();
+        git.branchDelete().setForce(true).setBranchNames(LOCAL_BRANCH).call();
+        git.checkout().setCreateBranch(true).setName(LOCAL_BRANCH).setStartPoint(version).call();
+    }
+
+    public String getCurrentVersion() throws IOException {
+        try (RevWalk walk = new RevWalk(repository)) {
+            Ref localBranch = repository.findRef("HEAD");
+            RevCommit headCommit = walk.lookupCommit(localBranch.getObjectId());
+            return headCommit.getName();
+        }
+    }
 
     public String commitUpload(String comment) throws IRemoteRepository2.NetworkException, GitAPIException {
         Log.logInfoRB("GIT_START", "upload");
@@ -49,7 +104,7 @@ public class GitCommit {
     public void commitVersion(String[] onVersions) throws IOException {
         if (onVersions != null) {
             // check versions
-            String currentVersion = new GITRemoteRepository2().getCurrentVersion();
+            String currentVersion = getCurrentVersion();
             boolean hasVersion = false;
             for (String v : onVersions) {
                 if (v != null) {
