4a2c0bc02f87f507ea506fe68711dbc4dd65011a
muhammad riansyah <mriansyah93@gmail.com>
2020-04-02 16:26:15 +0700

    extract class editorFilter

--- src/org/omegat/gui/editor/EditorController.java
+++ src/org/omegat/gui/editor/EditorController.java
@@ -147,7 +147,7 @@ import com.vlsolutions.swing.docking.DockingDesktop;
  * @author Piotr Kulik
  * @author Yu Tang
  */
-public class EditorController implements IEditor,IFilterForEditor {
+public class EditorController implements IEditor {
 
     /** Local logger. */
     private static final Logger LOGGER = Logger.getLogger(EditorController.class.getName());
@@ -159,8 +159,6 @@ public class EditorController implements IEditor,IFilterForEditor {
         UNTRANSLATED, EMPTY, EQUALS_TO_SOURCE;
     }
 
-    /** Dockable pane for editor. */
-    private DockablePanel pane;
     private JScrollPane scrollPane;
 
     private String title;
@@ -2031,98 +2029,6 @@ public class EditorController implements IEditor,IFilterForEditor {
         editor.registerPopupMenuConstructors(priority, constructor);
     }
 
-    @Override
-    public IEditorFilter getFilter() {
-        return entriesFilter;
-    }
-
-    /**
-     * instance variable
-     * entriesFilterControlComponent
-     * pane
-     * entriesFilter
-     *
-     * {@inheritDoc} Document is reloaded to immediately have the filter being
-     * effective.
-     */
-    public void setFilter(IEditorFilter filter) {
-        UIThreadsUtil.mustBeSwingThread();
-
-        if (entriesFilterControlComponent != null) {
-            pane.remove(entriesFilterControlComponent);
-        }
-
-        entriesFilter = filter;
-        entriesFilterControlComponent = filter.getControlComponent();
-        pane.add(entriesFilterControlComponent, BorderLayout.NORTH);
-        pane.revalidate();
-
-        preventNullLoad();
-    }
-
-    private void preventNullLoad() {
-        SourceTextEntry curEntry = getCurrentEntry();
-        Document3 doc = editor.getOmDocument();
-        IProject project = Core.getProject();
-        // Prevent NullPointerErrors in loadDocument. Only load if there is a document.
-        if (doc != null && project != null && project.getProjectFiles() != null && curEntry != null) {
-            int curEntryNum = curEntry.entryNum();
-            loadDocument(); // rebuild entrylist
-            if (entriesFilter == null || entriesFilter.allowed(curEntry)) {
-                gotoEntry(curEntry.entryNum());
-            } else {
-                nextSegmentEntry(curEntryNum);
-            }
-        }
-    }
-
-    private void nextSegmentEntry(int curEntryNum) {
-        // Go to next (available) segment. But first, we need to reset
-        // the displayedEntryIndex to the number where the current but
-        // filtered entry could have been if it was not filtered.
-        for (int j = 0; j < m_docSegList.length; j++) {
-            if (m_docSegList[j].segmentNumberInProject >= curEntryNum) { //
-                displayedEntryIndex = j - 1;
-                break;
-            }
-        }
-        nextEntry();
-    }
-
-    /**
-     * entriesFilter
-     * entriesFilterControlComponent
-     * pane
-     * {@inheritDoc} Document is reloaded if appropriate to immediately remove
-     * the filter;
-     */
-    public void removeFilter() {
-        UIThreadsUtil.mustBeSwingThread();
-
-        if (entriesFilter == null && entriesFilterControlComponent == null) {
-            return;
-        }
-
-        entriesFilter = null;
-        if (entriesFilterControlComponent != null) {
-            pane.remove(entriesFilterControlComponent);
-            pane.revalidate();
-            entriesFilterControlComponent = null;
-        }
-
-        int curEntryNum = getCurrentEntryNumber();
-        Document3 doc = editor.getOmDocument();
-        IProject project = Core.getProject();
-        // `if` check is to prevent NullPointerErrors in loadDocument.
-        // Only load if there is a document and the project is loaded.
-        if (doc != null && project != null && project.isProjectLoaded()) {
-            List<FileInfo> files = project.getProjectFiles();
-            if (files != null && !files.isEmpty()) {
-                loadDocument();
-                gotoEntry(curEntryNum);
-            }
-        }
-    }
 
     /**
      * {@inheritDoc}
